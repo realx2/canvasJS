4ba4b1d694e472dcac106b95e1a0f91b3cebb00d
a <throne81@kakao.com>
2021-09-23 23:57:54 +0900

    Set function : activemenu

---
+++ menu.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector(".menu__icon");
+const colors = document.querySelector(".controls-colors");
+
+function activeMenu() {
+  const actionMenu = menu.classList.add("active-menu");
+  if (actionMenu) 
+}
+
+if (menu) menu.addEventListener("click", activeMenu);
